18a5ae5d7c9d1305e7be627de531cbda27d33d8b
Djordje001 <dd20200077@student.fon.bg.ac.rs>
2024-07-10 01:04:56 +0200

    Omoguceno pokretanje server aplikacije

--- projekat/serverP/src/main/java/org/serverP/App.java
+++ projekat/serverP/src/main/java/org/serverP/App.java
@@ -1,13 +1,20 @@
 package org.serverP;
 
+import org.serverP.thread.ThreadServer;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+	private static ThreadServer threadServer;
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	if (threadServer == null || !threadServer.isAlive()) {
+			System.out.println("zelimo da upalimo");
+			threadServer = new ThreadServer();
+			threadServer.start();
+		}
     }
 }
