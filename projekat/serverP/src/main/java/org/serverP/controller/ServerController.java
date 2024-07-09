a958d7735af9bf3ba0c51b94e98308c3da8eb4e1
Djordje001 <dd20200077@student.fon.bg.ac.rs>
2024-07-09 22:21:49 +0200

    u server projektu dodate klase:ServerController,ThreadClient i ThreadServer

---
+++ projekat/serverP/src/main/java/org/serverP/controller/ServerController.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.serverP.controller;
+
+
+import java.util.ArrayList;
+
+
+
+
+/**
+ *
+ * @author Lenovo
+ */
+public class ServerController {
+
+    private static ServerController instance;
+
+    private ServerController() {
+    }
+
+    public static ServerController getInstance() {
+        if (instance == null) {
+            instance = new ServerController();
+        }
+        return instance;
+    }
+
+    
+    
+    
+   
+}
---
+++ projekat/serverP/src/main/java/org/serverP/thread/ThreadClient.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.serverP.thread;
+
+
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
+
+import org.zajednickiP.transfer.Request;
+import org.zajednickiP.transfer.Response;
+import org.zajednickiP.transfer.util.Operation;
+import org.zajednickiP.transfer.util.ResponseStatus;
+import org.serverP.controller.*;
+
+
+
+/**
+ *
+ * @author Lenovo
+ */
+public class ThreadClient extends Thread {
+
+    private Socket socket;
+
+    ThreadClient(Socket socket) {
+        this.socket = socket;
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (!socket.isClosed()) {
+                ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
+                Request request = (Request) in.readObject();
+                Response response = handleRequest(request);
+                ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
+                out.writeObject(response);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private Response handleRequest(Request request) {
+        Response response = new Response(null, null, ResponseStatus.Success);
+        try {
+            switch (request.getOperation()) {
+               
+             
+            
+                default:
+                    return null;
+            }
+        } catch (Exception e) {
+            response.setResponseStatus(ResponseStatus.Error);
+            response.setException(e);
+        }
+        return response;
+    }
+
+}
