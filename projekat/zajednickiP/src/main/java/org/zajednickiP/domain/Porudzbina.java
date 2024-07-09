e41580cd7a024b3fc1ceda1c0e1201221bbb2686
Djordje001 <dd20200077@student.fon.bg.ac.rs>
2024-07-09 19:42:20 +0200

    dodate domenske klase:Porudzbina i StavkaPorudzbine

---
+++ projekat/zajednickiP/src/main/java/org/zajednickiP/domain/Porudzbina.java
@@ -0,0 +1,287 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.zajednickiP.domain;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.Date;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Porudzbina extends AbstractDomainObject {
+
+    private Long porudzbinaID;
+    private Date datumVreme;
+    private Date datumIsporuke;
+    private String grad;
+    private String adresa;
+    private double cena;
+    private double popust;
+    private double konacnaCena;
+    private Kupac kupac;
+    private Administrator administrator;
+    private ArrayList<StavkaPorudzbine> stavkePorudzbine;
+
+    public Porudzbina(Long porudzbinaID, Date datumVreme, Date datumIsporuke, String grad, String adresa, double cena, double popust, double konacnaCena, Kupac kupac, Administrator administrator, ArrayList<StavkaPorudzbine> stavkePorudzbine) {
+      
+        setPorudzbinaID(porudzbinaID);
+     
+        setDatumVreme(datumVreme);
+    
+        setDatumIsporuke(datumIsporuke);
+   
+        setGrad(grad);
+   
+        setAdresa(adresa);
+    
+        setCena(cena);
+    
+        setPopust(popust);
+       
+        setKonacnaCena(konacnaCena);
+       
+        setKupac(kupac);
+      
+        setAdministrator(administrator);
+    
+        setStavkePorudzbine(stavkePorudzbine);
+    }
+
+    public Porudzbina() {
+    }
+
+    @Override
+    public String toString() {
+        return "Porudzbina{" + "datumVreme=" + datumVreme + ", datumIsporuke=" + datumIsporuke + ", kupac=" + kupac + '}';
+    }
+
+    
+   
+    
+    @Override
+    public String nazivTabele() {
+        return " Porudzbina ";
+    }
+
+    @Override
+    public String alijas() {
+        return " p ";
+    }
+
+    @Override
+    public String join() {
+        return " JOIN KUPAC K ON (K.KUPACID = P.KUPACID) "
+                + "JOIN ADMINISTRATOR A ON (A.ADMINISTRATORID = P.ADMINISTRATORID) ";
+        
+    }
+
+    @Override
+    public ArrayList<AbstractDomainObject> vratiListu(ResultSet rs) throws SQLException {
+        ArrayList<AbstractDomainObject> lista = new ArrayList<AbstractDomainObject>();
+
+        while (rs.next()) {
+            Administrator a = new Administrator(rs.getLong("AdministratorID"),
+                    rs.getString("a.Ime"), rs.getString("a.Prezime"),
+                    rs.getString("Username"), rs.getString("Password"));
+
+            Kupac k = new Kupac(rs.getLong("KupacID"),
+                    rs.getString("k.Ime"), rs.getString("k.Prezime"),
+                    rs.getString("email"), rs.getString("tipKupca"));
+
+            Porudzbina p = new Porudzbina(rs.getLong("porudzbinaID"),
+                    rs.getTimestamp("datumVreme"), rs.getDate("datumIsporuke"),
+                    rs.getString("grad"), rs.getString("adresa"),
+                    rs.getDouble("cena"), rs.getDouble("popust"),
+                    rs.getDouble("konacnaCena"), k, a, null);
+
+            lista.add(p);
+        }
+
+        rs.close();
+        return lista;
+    }
+
+    @Override
+    public String koloneZaInsert() {
+        return " (datumVreme, datumIsporuke, grad, adresa, cena, popust, konacnaCena, "
+                + "kupacID, administratorID) ";
+    }
+
+    @Override
+    public String uslov() {
+        return " porudzbinaID = " + porudzbinaID;
+    }
+
+    @Override
+    public String vrednostiZaInsert() {
+        return "'" + new Timestamp(datumVreme.getTime()) + "', "
+                + "'" + new java.sql.Date(datumIsporuke.getTime()) + "', "
+                + "'" + grad + "', '" + adresa + "', " + cena + ", " + popust + ", "
+                + konacnaCena + ", " + kupac.getKupacID() + ", " + administrator.getAdministratorID();
+    }
+
+    @Override
+    public String vrednostiZaUpdate() {
+        return " datumIsporuke = '" + new java.sql.Date(datumIsporuke.getTime()) + "', "
+                + "grad = '" + grad + "', "
+                + "adresa = '" + adresa + "', cena = " + cena + ", "
+                + "konacnaCena = " + konacnaCena;
+    }
+
+    @Override
+    public String uslovZaSelect() {
+       
+       /* if(kupac==null){
+            return "";
+        }
+        return "WHERE K.KUPACID= "+kupac.getKupacID();*/
+    	//return "WHERE CONCAT(K.IME,K.PREZIME)= "+kupac.getIme()+kupac.getPrezime();
+    	return "WHERE CONCAT(K.IME, ' ', K.PREZIME) = '" + kupac.getIme() + " " + kupac.getPrezime() + "'";
+        
+       
+    }
+
+    public Long getPorudzbinaID() {
+        return porudzbinaID;
+    }
+
+    public void setPorudzbinaID(Long porudzbinaID) {
+        this.porudzbinaID = porudzbinaID;
+    }
+
+    public Date getDatumVreme() {
+        return datumVreme;
+    }
+
+    public void setDatumVreme(Date datumVreme) {
+    	if(datumVreme==null) {
+    		throw new NullPointerException("datumVreme ne smeju biti null");
+    	}
+        this.datumVreme = datumVreme;
+    }
+
+    public Date getDatumIsporuke() {
+        return datumIsporuke;
+    }
+
+    public void setDatumIsporuke(Date datumIsporuke) {
+    	if(datumIsporuke==null) {
+    		throw new NullPointerException("datumIsporuke ne sme biti null");
+    		
+    	}
+    	
+    	Date trenutniDatum=new Date();
+    	if(datumIsporuke.before(trenutniDatum)) {
+    		throw new IllegalArgumentException("datumIsporuke mora biti u buducnosti");
+    	}
+        this.datumIsporuke = datumIsporuke;
+    }
+
+    public String getGrad() {
+        return grad;
+    }
+
+    public void setGrad(String grad) {
+    	if(grad==null) {
+    		throw new NullPointerException("grad ne sme biti null");
+    	}
+    	if(grad.equals("")) {
+    		throw new IllegalArgumentException("grad ne sme biti prazan string");
+    	}
+        this.grad = grad;
+    }
+
+    public String getAdresa() {
+        return adresa;
+    }
+
+    public void setAdresa(String adresa) {
+    	if(adresa==null) {
+    		throw new NullPointerException("adresa ne sme biti null");
+    	}
+    	if(adresa.equals("")) {
+    		throw new IllegalArgumentException("adresa ne sme biti prazan string");
+    	}
+        this.adresa = adresa;
+    }
+
+    public double getCena() {
+        return cena;
+    }
+
+    public void setCena(double cena) {
+    	if(cena<=0) {
+    		throw new IllegalArgumentException("cena mora biti veca od 0");
+    	}
+    	
+        this.cena = cena;
+    }
+
+    public double getPopust() {
+        return popust;
+    }
+
+    public void setPopust(double popust) {
+    	if(popust<0) {
+    		throw new IllegalArgumentException("popust ne sme biti negativan");
+    	}
+    	
+        this.popust = popust;
+    }
+
+    public double getKonacnaCena() {
+        return konacnaCena;
+    }
+
+    public void setKonacnaCena(double konacnaCena) {
+    	if(konacnaCena<=0) {
+    		throw new IllegalArgumentException("konacnaCena mora biti pozitivna");
+    	}
+    	
+        this.konacnaCena = konacnaCena;
+    }
+
+    public Kupac getKupac() {
+        return kupac;
+    }
+
+    public void setKupac(Kupac kupac) {
+    	if(kupac==null) {
+    		throw new NullPointerException("kupac ne sme biti null");
+    	}
+        this.kupac = kupac;
+    }
+
+    public Administrator getAdministrator() {
+        return administrator;
+    }
+
+    public void setAdministrator(Administrator administrator) {
+    	if(administrator==null) {
+    		throw new NullPointerException("administrator ne sme biti null");
+    	}
+        this.administrator = administrator;
+    }
+
+    public ArrayList<StavkaPorudzbine> getStavkePorudzbine() {
+        return stavkePorudzbine;
+    }
+
+    public void setStavkePorudzbine(ArrayList<StavkaPorudzbine> stavkePorudzbine) {
+    	if(stavkePorudzbine==null) {
+    		throw new NullPointerException("stavke ne smeju biti null");
+    	}
+    	if(stavkePorudzbine.isEmpty()) {
+    		throw new IllegalArgumentException("porudzbina mora sadrzati barem jednu stavku");
+    	}
+        this.stavkePorudzbine = stavkePorudzbine;
+    }
+
+}
---
+++ projekat/zajednickiP/src/main/java/org/zajednickiP/domain/StavkaPorudzbine.java
@@ -0,0 +1,191 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.zajednickiP.domain;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class StavkaPorudzbine extends AbstractDomainObject {
+
+	
+    private transient Porudzbina porudzbina;
+    private int rb;
+    private int kolicina;
+    private double cena;
+    private Proizvod proizvod;
+
+    public StavkaPorudzbine(Porudzbina porudzbina, int rb, int kolicina, double cena, Proizvod proizvod) {
+       
+        setPorudzbina(porudzbina);
+       
+        setRb(rb);
+        
+        setKolicina(kolicina);
+      
+        setCena(cena);
+      
+        setProizvod(proizvod);
+    }
+
+    public StavkaPorudzbine() {
+    }
+
+    @Override
+    public String nazivTabele() {
+        return " StavkaPorudzbine ";
+    }
+
+    @Override
+    public String alijas() {
+        return " sp ";
+    }
+
+    @Override
+    public String join() {
+        return " JOIN PROIZVOD PR ON (PR.PROIZVODID = SP.PROIZVODID) "
+                + "JOIN PORUDZBINA P ON (P.PORUDZBINAID = SP.PORUDZBINAID) "
+                + "JOIN KUPAC K ON (K.KUPACID = P.KUPACID) "
+                + "JOIN ADMINISTRATOR A ON (A.ADMINISTRATORID = P.ADMINISTRATORID) "
+                +"LEFT JOIN KANCELARIJSKIPROIZVOD KM ON (PR.TIP = 1 AND KM.PROIZVODID = SP.PROIZVODID) "
+                +"LEFT JOIN KNJIGA KJ ON (PR.TIP = 2 AND KJ.PROIZVODID = SP.PROIZVODID) ";
+    }
+
+    @Override
+    public ArrayList<AbstractDomainObject> vratiListu(ResultSet rs) throws SQLException {
+        ArrayList<AbstractDomainObject> lista = new ArrayList<AbstractDomainObject>();
+
+        while (rs.next()) {
+            Administrator a = new Administrator(rs.getLong("AdministratorID"),
+                    rs.getString("a.Ime"), rs.getString("a.Prezime"),
+                    rs.getString("Username"), rs.getString("Password"));
+
+            Kupac k = new Kupac(rs.getLong("KupacID"),
+                    rs.getString("k.Ime"), rs.getString("k.Prezime"),
+                    rs.getString("email"), rs.getString("tipKupca"));
+
+            Porudzbina p = new Porudzbina(rs.getLong("porudzbinaID"),
+                    rs.getTimestamp("datumVreme"), rs.getDate("datumIsporuke"),
+                    rs.getString("grad"), rs.getString("adresa"),
+                    rs.getDouble("cena"), rs.getDouble("popust"),
+                    rs.getDouble("konacnaCena"), k, a, null);
+            
+            //public Knjiga(Long proizvodID, double cena, String naziv,int tip,int izdanje,String opis,ArrayList<Autor> autori)
+          //  public KancelarijskiProizvod(Long proizvodID, double cena, String naziv,int tip,String vrsta,String proizvodjac,double visina,double sirina,double duzina)
+            StavkaPorudzbine sp=new StavkaPorudzbine();
+            if(rs.getInt("tip")==1) {
+            	KancelarijskiProizvod kp=new KancelarijskiProizvod(rs.getLong("proizvodID"), rs.getDouble("cena"), rs.getString("naziv"),
+            			rs.getInt("tip"), rs.getString("vrsta"), rs.getString("proizvodjac"), rs.getDouble("visina"),rs.getDouble("sirina"), rs.getDouble("duzina"));
+            	sp=new StavkaPorudzbine(p, rs.getInt("rb"), rs.getInt("kolicina"), rs.getDouble("cena"), kp);
+            }else {
+            	 Knjiga knj = new Knjiga(rs.getLong("proizvodID"),
+                         rs.getDouble("cena"), rs.getString("naziv"),
+                         rs.getInt("tip"), rs.getInt("izdanje"),rs.getString("opis"), null);
+            	 sp = new StavkaPorudzbine(p, rs.getInt("rb"), 
+                         rs.getInt("kolicina"), rs.getDouble("cena"), knj);
+            }
+            
+            lista.add(sp);
+        }
+
+        rs.close();
+        return lista;
+    }
+
+    @Override
+    public String koloneZaInsert() {
+        return " (porudzbinaID, kolicina, cena, proizvodID) ";
+    }
+
+    @Override
+    public String uslov() {
+        return " porudzbinaID = " + porudzbina.getPorudzbinaID();
+    }
+
+    @Override
+    public String vrednostiZaInsert() {
+        return " " + porudzbina.getPorudzbinaID() + ", "
+                + " " + kolicina + ", " + cena + ", " + proizvod.getProizvodID();
+    }
+
+    @Override
+    public String vrednostiZaUpdate() {
+        return "";
+    }
+
+    @Override
+    public String uslovZaSelect() {
+        return " WHERE P.PORUDZBINAID = " + porudzbina.getPorudzbinaID();
+    }
+
+    public Porudzbina getPorudzbina() {
+        return porudzbina;
+    }
+
+    public void setPorudzbina(Porudzbina porudzbina) {
+    	if(porudzbina==null) {
+    		throw new NullPointerException("porudzbina ne sme biti null");
+    	}
+        this.porudzbina = porudzbina;
+    }
+
+    public int getRb() {
+        return rb;
+    }
+
+    public void setRb(int rb) {
+    	//nemam proveru vrednosti jer mi je rb autoincrement u bazi znaci ja ne zadajem vrednost
+        this.rb = rb;
+    }
+
+    public int getKolicina() {
+        return kolicina;
+    }
+
+    public void setKolicina(int kolicina) {
+    	if(kolicina<=0) {
+    		throw new IllegalArgumentException("kolicina mora biti pozitivna");
+    	}
+        this.kolicina = kolicina;
+    }
+
+    public double getCena() {
+        return cena;
+    }
+
+    public void setCena(double cena) {
+    	if(cena<=0) {
+    		throw new IllegalArgumentException("cena mora biti pozitivna");
+    	}
+        this.cena = cena;
+    }
+
+    public Proizvod getProizvod() {
+        return proizvod;
+    }
+
+    public void setProizvod(Proizvod proizvod) {
+    	if(proizvod==null) {
+    		throw new IllegalArgumentException("proizvod ne sme biit null");
+    	}
+        this.proizvod = proizvod;
+    }
+
+	@Override
+	public String toString() {
+		return "StavkaPorudzbine [porudzbina=" + porudzbina + ", rb=" + rb + ", kolicina=" + kolicina + ", cena=" + cena
+				+ ", proizvod=" + proizvod + "]";
+	}
+    
+    
+    
+    
+
+}
