869828a14d30ae569be9598cc74f1466e534819d
Djordje001 <dd20200077@student.fon.bg.ac.rs>
2024-07-09 22:25:41 +0200

    Dodate klase AbstractSO i DBBroker

---
+++ projekat/serverP/src/main/java/org/serverP/db/DBBroker.java
@@ -0,0 +1,106 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.serverP.db;
+
+
+import java.io.FileInputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Properties;
+
+import org.zajednickiP.domain.AbstractDomainObject;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class DBBroker {
+
+    private static DBBroker instance;
+    private Connection connection;
+
+    private DBBroker() {
+        try {
+           // Properties properties = new Properties();
+         //   properties.load(new FileInputStream("dbconfig.properties"));
+           // String url = properties.getProperty("url");
+         //   String username = properties.getProperty("korisnickoIme");
+           // String password = properties.getProperty("sifra");
+        	String url = "jdbc:mysql://localhost:3306/proba";
+        	String username="root";
+        	String password="";
+            connection = DriverManager.getConnection(url, username, password);
+            connection.setAutoCommit(false);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public static DBBroker getInstance() {
+        if (instance == null) {
+            instance = new DBBroker();
+        }
+        return instance;
+    }
+
+    public ArrayList<AbstractDomainObject> select(AbstractDomainObject ado) throws SQLException {
+        String upit = "SELECT * FROM " + ado.nazivTabele() + " " + ado.alijas()
+                + " " + ado.join() + " ";
+        System.out.println(upit);
+        Statement s = connection.createStatement();
+        ResultSet rs = s.executeQuery(upit);
+        return ado.vratiListu(rs);
+    }
+    
+    public ArrayList<AbstractDomainObject> find(AbstractDomainObject ado) throws SQLException {
+        String upit = "SELECT * FROM " + ado.nazivTabele() + " " + ado.alijas()
+                + " " + ado.join() + " " + ado.uslovZaSelect();
+        System.out.println(upit);
+        Statement s = connection.createStatement();
+        ResultSet rs = s.executeQuery(upit);
+        return ado.vratiListu(rs);
+    }
+
+    public PreparedStatement insert(AbstractDomainObject ado) throws SQLException {
+        String upit = "INSERT INTO " + ado.nazivTabele() + " "
+                + ado.koloneZaInsert() + " VALUES(" + ado.vrednostiZaInsert() + ")";
+        System.out.println(upit);
+        PreparedStatement ps = connection.prepareStatement(upit, Statement.RETURN_GENERATED_KEYS);
+        ps.executeUpdate();
+        return ps;
+    }
+
+    public void update(AbstractDomainObject ado) throws SQLException {
+        String upit = "UPDATE " + ado.nazivTabele() + " SET "
+                + ado.vrednostiZaUpdate() + " WHERE " + ado.uslov();
+        System.out.println(upit);
+        Statement s = connection.createStatement();
+        s.executeUpdate(upit);
+    }
+
+    public void delete(AbstractDomainObject ado) throws SQLException {
+        String upit = "DELETE FROM " + ado.nazivTabele() + " WHERE " + ado.uslov();
+        System.out.println(upit);
+        Statement s = connection.createStatement();
+        s.executeUpdate(upit);
+    }
+    public void deleteAll(AbstractDomainObject ado) throws SQLException{
+    	String upit="DELETE FROM "+ado.nazivTabele();
+    	System.out.println(upit);
+    	Statement s=connection.createStatement();
+    	s.executeUpdate(upit);
+    }
+
+}
